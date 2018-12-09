43cf57cca8cc2911bcf0653b16329a5c764c7910
Kseniia Shavonina <kshavonina@yandex.ru>
2018-12-09 03:12:40 +0300

    Initial commit

---
+++ src/main/java/com/example/budgetcheckerweb/BudgetcheckerwebApplication.java
@@ -0,0 +1,12 @@
+package com.example.budgetcheckerweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetcheckerwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetcheckerwebApplication.class, args);
+	}
+}
